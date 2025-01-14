17e5b90fec9d4ac1609f18a32039559b02b7af27
Reece <you@example.com>
2025-01-14 14:38:47 +0000

    Signed-off-by: Reece <you@example.com>

--- Pages/Logs.cshtml.cs
+++ Pages/Logs.cshtml.cs
@@ -1,23 +1,28 @@
-using KothBackend.Models;
-using KothBackend.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Antiforgery;
+using KothBackend.Services;
+using KothBackend.Models;
 
 namespace KothBackend.Pages
 {
     public class LogsModel : PageModel
     {
         private readonly IRequestLogService _logService;
+        private readonly IAntiforgery _antiforgery;
 
         public IEnumerable<RequestLog> Logs { get; private set; } = new List<RequestLog>();
 
-        public LogsModel(IRequestLogService logService)
+        public LogsModel(IRequestLogService logService, IAntiforgery antiforgery)
         {
             _logService = logService;
+            _antiforgery = antiforgery;
         }
 
         public void OnGet()
         {
+            var tokens = _antiforgery.GetAndStoreTokens(HttpContext);
+            Response.Headers.Add("X-CSRF-TOKEN", tokens.RequestToken);
             Logs = _logService.GetLogs();
         }
 
